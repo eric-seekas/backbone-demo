4a40388dc811e6d188ecaecc8af3ef6845c4fcaa
dsky1990 <dutian1990@gmail.com>
2016-09-19 17:09:38 +0800

    update file

--- gulpfile.js
+++ gulpfile.js
@@ -18,24 +18,38 @@ const babel = require('gulp-babel'); // es6 to js
 const eslint = require('gulp-eslint'); // eslint
 const browserSync = require('browser-sync').create(); // browserSync
 const replace = require('gulp-replace'); // gulp-replace
-
-// eslint
+const stripDebug = require('gulp-strip-debug'); // Strip console, alert, and debugger
+const pump = require('pump');
+const uglify = require('gulp-uglify');// uglify js
 
 // file path
 const buildPug = 'build/pug/*.pug';
 const buildPugTpl = 'build/pug/pug-tpl/*.pug';
 const build = 'build/';
 const buildHtml = 'build/*.html';
+const buildJs = 'build/js/*.js';
+const vendorCss = 'vendor/Framework7/dist/css/*.css';
+const distJs = 'dist/js/*.js';
+const distJsSrc = 'dist/js';
 const dir = './';
 const dirHtml = './*.html';
 
 // task
+// eslint
+gulp.task('eslint', () => gulp.src(buildJs).pipe(changed(buildJs)).pipe(stripDebug()).pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()));
+// es6 to js
+// js minify
+gulp.task('jscompress', () => pump([gulp.src(buildJs), stripDebug(), uglify(), gulp.dest(distJsSrc)]));
 // pug to html
 gulp.task('pug-to-html', () => gulp.src(buildPug).pipe(pug({ pretty: true, self: true })).pipe(gulp.dest(build)));
 // htmlmin
 gulp.task('htmlmin', () => gulp.src(buildHtml).pipe(changed(buildHtml)).pipe(htmlmin({ collapseWhitespace: true })).pipe(gulp.dest(dir)));
 // replace
 gulp.task('replace', () => gulp.src(dirHtml).pipe(replace('css/style.css', 'dist/css/style.css').pipe(gulp.dest(dir))));
+// purifycss
+gulp.task('purifycss', () => gulp.src(vendorCss).pipe(purifycss([dirHtml, distJs]).pipe(gulp.dest('dist/css/style.min.css'))));
 
 // develop
 gulp.task('pug-watch', () => gulp.watch((buildPug, buildPugTpl), ['pug-to-html']));
