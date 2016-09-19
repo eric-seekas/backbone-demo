a6e19ace9a25cb60b2620caf801ea07d14f96b54
dsky1990 <dutian1990@gmail.com>
2016-09-19 15:45:56 +0800

    update file

--- gulpfile.js
+++ gulpfile.js
@@ -17,13 +17,23 @@ const htmlmin = require('gulp-htmlmin'); // purify html
 const babel = require('gulp-babel'); // es6 to js
 const eslint = require('gulp-eslint'); // eslint
 const browserSync = require('browser-sync').create(); // browserSync
+
 // eslint
+
 // file path
 const buildPug = 'build/pug/*.pug';
 const build = 'build/';
+const buildHtml = 'build/*.html';
+const dir = './';
+
 // task
 // pug to html
-gulp.task('pug-to-html', () => gulp.src(buildPug).pipe(changed(buildPug)).pipe(pug({ pretty: true, self: true})).pipe(gulp.dest(build)));
+gulp.task('pug-to-html', () => gulp.src(buildPug).pipe(changed(buildPug)).pipe(pug({ pretty: true, self: true })).pipe(gulp.dest(build)));
+// htmlmin
+gulp.task('htmlmin', () => gulp.src(buildHtml).pipe(changed(buildHtml)).pipe(htmlmin({ collapseWhitespace: true })).pipe(gulp.dest(dir)));
+
 // develop
 gulp.task('pug-watch', () => gulp.watch(buildPug, ['pug-to-html']));
-// official
+
+// release
+gulp.task('release', ['htmlmin']);
